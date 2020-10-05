51944646107280c8f26ac418eff879c914665821
theKernel <tf875427@gmail.com>
2020-10-05 20:53:41 +0200

    05 October 2020

--- Modern JS/AsynchronousJS/async.js
+++ Modern JS/AsynchronousJS/async.js
@@ -3,85 +3,85 @@ window.onload = function (){
     
 };
 
-// function* gen(){
-//     let x = yield 10;
-//     console.log(x);
-// }
+function* gen(){
+    let x = yield 10;
+    console.log(x);
+}
 
-// let myGen = gen();
-// console.log(myGen.next());
-// console.log(myGen.next(10));
+let myGen = gen();
+console.log(myGen.next());
+console.log(myGen.next(10));
 
-// function handleError(jqXHR, textStatus, error){
-//     console.log(error);
-// }
+function handleError(jqXHR, textStatus, error){
+    console.log(error);
+}
 
-// $.ajax({
-//   type: 'GET',
-//   url: 'tweets.json',
-//   success: cbTweets,
-//   error: handleError
-// });
+$.ajax({
+  type: 'GET',
+  url: 'tweets.json',
+  success: cbTweets,
+  error: handleError
+});
 
-// function cbTweets(data){
-//     console.log(data);
+function cbTweets(data){
+    console.log(data);
 
-// $.ajax({
-//     type: 'GET',
-//     url: 'friends.json',
-//     success: cbFriends
-//     error: handleError
-//     });
+$.ajax({
+    type: 'GET',
+    url: 'friends.json',
+    success: cbFriends
+    error: handleError
+    });
 
-//     }
+    }
     
-// function cbFriends(data){
-//      console.log(data);
+function cbFriends(data){
+     console.log(data);
 
-//         $.ajax({
-//             type: 'GET',
-//             url: 'videos.json',
-//             success: function (data){
-//                 console.log(data);
+        $.ajax({
+            type: 'GET',
+            url: 'videos.json',
+            success: function (data){
+                console.log(data);
       
-//             },
-//             error: handleError
-//           });
-//         }
+            },
+            error: handleError
+          });
+        }
 
 
-// function cb(data){
-//     console.log(data);
-// }
+function cb(data){
+    console.log(data);
+}
 
-// $.get("tweets.json", cb);
+$.get("tweets.json", cb);
+console.log("test");
+
+
+
+
+function callback(val){
+    console.log(val);
+}
+
+let fruits = ["banana", "apple", "pear"];
+fruits.forEach(callback);
+console.log("done");
+let http = new XMLHttpRequest();
+
+http.onreadystatechange = function (){
+    if(http.readyState == 4 && http.status == 200){
+        //console.log(JSON.parse(http.responseText));
+    }
+};
+    
+
+http.open("GET", "tweets.json", true);
+http.send();
 // console.log("test");
 
-
-
-
-// function callback(val){
-//     console.log(val);
-// }
-
-// let fruits = ["banana", "apple", "pear"];
-// fruits.forEach(callback);
-// console.log("done");
-// let http = new XMLHttpRequest();
-
-// http.onreadystatechange = function (){
-//     if(http.readyState == 4 && http.status == 200){
-//         //console.log(JSON.parse(http.responseText));
-//     }
-// };
-    
-
-// http.open("GET", "tweets.json", true);
-// http.send();
-// // console.log("test");
-
-// $.get("tweets.json", function (data){
-//     console.log(data);
-// });
-// console.log("test")
-// };
+$.get("tweets.json", function (data){
+    console.log(data);
+});
+console.log("test")
+};
